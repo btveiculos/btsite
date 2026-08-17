5558bca84765d083d68615d2a1e1f69c8db80e42
btveiculos <btveiculosjundiai@gmail.com>
2026-08-17 17:04:51 -0300

    Atualização estoque via painel

--- js/data.js
+++ js/data.js
@@ -106,7 +106,7 @@ const VEHICLES = [
     "combustivel": "Flex",
     "cor": "Laranja",
     "preco": 43900,
-    "img": "/carros/fiat-uno-2012-4.jpg",
+    "img": "/carros/fiat-uno-2012-5.jpg",
     "heroImg": "",
     "opcionais": [
       "Ar-condicionado",
@@ -115,7 +115,6 @@ const VEHICLES = [
     ],
     "desc": "Uno Sporting 1.4 completo. Esportivo e econômico.",
     "fotos": [
-      "/carros/fiat-uno-2012-4.jpg",
       "/carros/fiat-uno-2012-5.jpg",
       "/carros/fiat-uno-2012-6.jpg",
       "/carros/fiat-uno-2012-7.jpg",
