bfe9d55c11dcb5a3f39bba8f234c6cc333a80168
btveiculos <btveiculosjundiai@gmail.com>
2026-08-19 14:54:48 -0300

    Atualização estoque via painel

--- js/data.js
+++ js/data.js
@@ -292,7 +292,7 @@ const VEHICLES = [
     "combustivel": "Flex",
     "cor": "Branco",
     "preco": 63850,
-    "img": "/carros/chevrolet-onix-2021-1.jpg",
+    "img": "/carros/chevrolet-onix-2021-6.jpg",
     "heroImg": "/carros/onix-hero.png",
     "opcionais": [
       "Ar-condicionado",
@@ -302,12 +302,12 @@ const VEHICLES = [
     ],
     "desc": "Onix Hatch LT 1.0. Moderno e econômico.",
     "fotos": [
+      "/carros/chevrolet-onix-2021-6.jpg",
       "/carros/chevrolet-onix-2021-1.jpg",
       "/carros/chevrolet-onix-2021-2.jpg",
       "/carros/chevrolet-onix-2021-3.jpg",
       "/carros/chevrolet-onix-2021-4.jpg",
       "/carros/chevrolet-onix-2021-5.jpg",
-      "/carros/chevrolet-onix-2021-6.jpg",
       "/carros/chevrolet-onix-2021-7.jpg",
       "/carros/chevrolet-onix-2021-8.jpg",
       "/carros/chevrolet-onix-2021-9.jpg"
