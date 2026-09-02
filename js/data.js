33c0329711c8aa976604a241ebb25d398a7d912b
btveiculos <btveiculosjundiai@gmail.com>
2026-09-02 17:38:56 -0300

    Atualização estoque via painel

--- js/data.js
+++ js/data.js
@@ -712,7 +712,7 @@ const VEHICLES = [
     "combustivel": "Flex",
     "cor": "Preto",
     "preco": 72500,
-    "img": "/carros/chevrolet-onix-plus-2024-1787341755161-1.jpg",
+    "img": "/carros/chevrolet-onix-plus-2024-1788381490524-1.jpg",
     "heroImg": "",
     "opcionais": [
       "Ar-condicionado",
@@ -722,8 +722,15 @@ const VEHICLES = [
     ],
     "desc": "Onix Plus LT 1.0 completo. Sedan moderno.",
     "fotos": [
-      "/carros/chevrolet-onix-plus-2024-1787341755161-1.jpg",
-      "/carros/chevrolet-onix-plus-2024-1787341817106-1.jpg"
+      "/carros/chevrolet-onix-plus-2024-1788381490524-1.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-2.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-3.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-4.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-5.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-6.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-7.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-8.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788381490524-9.jpg"
     ]
   },
   {
