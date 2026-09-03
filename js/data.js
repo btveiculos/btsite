595708fe18a88d400be40d741965c2b79da8c024
btveiculos <btveiculosjundiai@gmail.com>
2026-09-03 11:22:41 -0300

    Atualização estoque via painel

--- js/data.js
+++ js/data.js
@@ -725,13 +725,13 @@ const VEHICLES = [
       "/carros/chevrolet-onix-plus-2024-1788382878311-9.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-1.jpg",
       "/carros/chevrolet-onix-plus-2024-1787341755161-1.jpg",
+      "/carros/chevrolet-onix-plus-2024-1788382878311-7.jpg",
       "/carros/chevrolet-onix-plus-2024-1787341817106-1.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-2.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-3.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-4.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-5.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-6.jpg",
-      "/carros/chevrolet-onix-plus-2024-1788382878311-7.jpg",
       "/carros/chevrolet-onix-plus-2024-1788382878311-8.jpg"
     ]
   },
